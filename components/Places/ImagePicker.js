2547e7052d2107d5c827f694b91d93212631edf1
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-09-11 01:09:57 +0300

    showing an image preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,24 +1,58 @@
-import { StyleSheet, View, Button } from 'react-native'
+import { StyleSheet, View, Button, Alert, Image, Text } from 'react-native'
 import React from 'react'
-import { launchCameraAsync } from 'expo-image-picker'
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker'
+import { Colors } from '../../constants/colors'
 
 const ImagePicker = () => {
 
+    const [pickedImage, setPickedImage] = React.useState("")
+
+    const [cameraPermissionInformation, requestPermission] = useCameraPermissions()
+
+    async function verifyPermissions() {
+
+        if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestPermission();
+            return permissionResponse.granted
+        }
+        if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+            Alert.alert('Insufficient Permissions!', "You need to grant camera permissions to use this app.")
+            return false
+        }
+
+        return true
+    }
+
     async function takeImageHandler() {
+
+        const hasPermission = await verifyPermissions()
+
+        if (!hasPermission) {
+            return
+        }
+
         const image = await launchCameraAsync({
             allowsEditing: true,
             aspect: [16, 9],
             quality: 0.5,
         })
+
         console.log("image", image)
+        setPickedImage(image)
+
     }
 
 
-
+    let imagePreview = <Text style={{ color: 'white ' }}>No image Taken yet</Text>
+    if (pickedImage) {
+        imagePreview = <Image style={styles.image} source={{ uri: pickedImage.uri }} />
+    }
 
     return (
         <View>
-            <View></View>
+            <View style={styles.imagePreview}>
+                {imagePreview}
+            </View>
             <Button title={"Take image"} onPress={takeImageHandler} />
         </View>
     )
@@ -26,4 +60,18 @@ const ImagePicker = () => {
 
 export default ImagePicker
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+})
